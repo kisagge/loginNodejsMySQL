79c14d9d1803cd1e7e4ee4e1183b70b9bfddda8a
Jang Byung Yeon <kisagge@naver.com>
2021-06-08 22:21:15 +0900

    main

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db');
+var tweet = require('./lib/tweet');
 var login = require('./lib/login');
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -11,10 +13,22 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        login.login(request, response);
+        tweet.index(request, response);
       } else {
 
       }
+    } else if(pathname === '/login'){
+      login.login(request, response);
+    } else if(pathname === '/login_process'){
+      login.login_process(request, response);
+    } else if(pathname === '/login_failure'){
+      login.login_failure(request, response);
+    } else if(pathname === '/signup'){
+      login.signup(request, response);
+    } else if(pathname === '/signup_process'){
+      login.signup_process(request, response);
+    } else if(pathname === '/signup_failure'){
+      login.signup_failure(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
